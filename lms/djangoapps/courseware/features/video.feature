22cb0e90e64710c4ceb8b0df609e23d895d07516
Anton Stupak <s2pak.anton@gmail.com>
2013-08-07 15:49:44 +0300

    Fix tests

--- lms/djangoapps/courseware/features/video.feature
+++ lms/djangoapps/courseware/features/video.feature
@@ -1,14 +1,18 @@
 Feature: Video component
   As a student, I want to view course videos in LMS.
 
-  Scenario: Videoalpha component is fully rendered in the LMS
-  Given the course has a VideoAlpha component
-  Then when I view the videoalpha it has video element
+  Scenario: Videoalpha component is fully rendered in the LMS in HTML5 mode
+  Given the course has a VideoAlpha in HTML5 mode component
+  Then when I view the videoalpha it has rendered in HTML5 mode
+
+  Scenario: Videoalpha component is fully rendered in the LMS in Youtube mode
+  Given the course has a VideoAlpha in Youtube mode component
+  Then when I view the videoalpha it has rendered in Youtube mode
 
   Scenario: Autoplay is enabled in LMS for a Video component
   Given the course has a Video component
   Then when I view the video it has autoplay enabled
 
   Scenario: Autoplay is enabled in the LMS for a VideoAlpha component
-  Given the course has a VideoAlpha component
+  Given the course has a VideoAlpha in Youtube mode component
   Then when I view the videoalpha it has autoplay enabled
--- lms/djangoapps/courseware/features/video.py
+++ lms/djangoapps/courseware/features/video.py
@@ -32,13 +32,13 @@ def view_video(_step):
     world.browser.visit(url)
 
 
-@step('the course has a VideoAlpha component')
-def view_videoalpha(step):
+@step('the course has a VideoAlpha in (.*) mode component')
+def view_videoalpha(step, player_mode):
     coursenum = 'test_course'
     i_am_registered_for_the_course(step, coursenum)
 
     # Make sure we have a videoalpha
-    add_videoalpha_to_course(coursenum)
+    add_videoalpha_to_course(coursenum, player_mode.lower())
     chapter_name = world.scenario_dict['SECTION'].display_name.replace(" ", "_")
     section_name = chapter_name
     url = django_url('/courses/%s/%s/%s/courseware/%s/%s' %
@@ -48,19 +48,45 @@ def view_videoalpha(step):
 
 
 def add_video_to_course(course):
-    world.ItemFactory.create(parent_location=section_location(course),
-                             category='video',
-                             display_name='Video')
+    world.ItemFactory.create(
+        parent_location=section_location(course),
+        category='video',
+        display_name='Video',
+    )
 
 
-def add_videoalpha_to_course(course):
+def add_videoalpha_to_course(course, player_mode):
     category = 'videoalpha'
-    world.ItemFactory.create(parent_location=section_location(course),
-                             category=category,
-                             display_name='Video Alpha')
+    metadata = {
+        'youtube_id_1_0':'',
+        'youtube_id_0_75':'',
+        'youtube_id_1_25':'',
+        'youtube_id_1_5':'',
+        'html5_sources':[
+            'https://s3.amazonaws.com/edx-course-videos/edx-intro/edX-FA12-cware-1_100.mp4'
+            'https://s3.amazonaws.com/edx-course-videos/edx-intro/edX-FA12-cware-1_100.webm'
+            'https://s3.amazonaws.com/edx-course-videos/edx-intro/edX-FA12-cware-1_100.ogv'
+        ],
+    } if (player_mode is 'html5') \
+    else {
+        'youtube_id_1_0':'3O1_3zBUKM8',
+        'youtube_id_0_75':'3O1_3zBUKM8',
+        'youtube_id_1_25':'3O1_3zBUKM8',
+        'youtube_id_1_5':'3O1_3zBUKM8',
+    }
+    world.ItemFactory.create(
+        parent_location=section_location(course),
+        category=category,
+        display_name='Video Alpha',
+        metadata=metadata
+    )
 
 
-@step('when I view the videoalpha it has rendered')
-def does_videoalpha_rendered(_step):
-    assert world.is_css_present('.videoalpha')
+@step('when I view the videoalpha it has rendered in HTML5 mode')
+def does_videoalpha_rendered_in_html5_mode(_step):
+    assert world.is_css_present('.videoalpha .video-player video')
+
+@step('when I view the videoalpha it has rendered in Youtube mode')
+def does_videoalpha_rendered_in_youtube_mode(_step):
+    assert world.is_css_present('.videoalpha .video-player iframe')
 
